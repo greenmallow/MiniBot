25517f8fb39b27a6af146de6fb0e1aa532bb481f
greenmallow <dmarmion08@gmail.com>
2021-01-19 19:04:45 +1100

    Add help command

--- bot.py
+++ bot.py
@@ -2,10 +2,12 @@ import discord
 
 from secrets import BOT_TOKEN
 
-client = discord.Client()
 
 BOT_NAME = 'Minimally Entertaining Bot' # Bot's name in sent messages
 PREFIX = '$$' # The prefix bot commands will start with
+COMMANDS = ('help',)
+
+client = discord.Client()
 
 
 @client.event
@@ -23,9 +25,21 @@ async def on_message(message):
     if message.content.startswith(PREFIX):
         # msg represents the message with the prefix removed
         msg = message.content[len(PREFIX):]
+        tokens = msg.split()
+
+        if len(tokens) > 0:
+            command = tokens[0]
+            arguments = tokens[1:]
+
+            if command == 'help':
+                # Sends a list of all available commands
+                preamble = f"The following commands are available. (All commands are prefixed with '{PREFIX}')"
+                response = f"{preamble}\n`{str(COMMANDS)[1:-1]}`"
+                    
+                await message.channel.send(response)
 
     elif message.content.lower().startswith("i'm "):
-        # Implements the 'Hi hungry, I'm Dad'-type joke
+        # Implements the "Hi hungry, I'm Dad" style joke
         iam = message.content[len("I'm "):]
         await message.channel.send(f"Hi {iam}, I'm {BOT_NAME}!")
 
